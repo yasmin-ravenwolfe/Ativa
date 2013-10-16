3e2d81fef9ed42fd8291d032ee32a43acb9551e5
MakerSquare Student <students@themakersquare.com>
2013-10-16 12:15:42 -0500

    Add failing email upon new post method; project is not being recognized across controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,7 @@ class PostsController < ApplicationController
     @post.phase_id = Phase.find_by_name(params[:post][:phase_id]).id
     respond_to do |format|
       if @post.save
+        Notifier.post_notification(current_user, @project, @post).deliver
         format.html { redirect_to project_url(@post.project_id, phase: @post.phase.name), notice: 'Post was successfully created.' }
         format.json { render action: 'show', status: :created, location: @post }
       else
--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -19,4 +19,13 @@ class Notifier < ActionMailer::Base
 
     mail(to: "#{user.name} <#{user.email}>", subject: "Un-subscribed to #{project.name.capitalize} | Ativa")
   end
+
+  def post_notification(user, project, post)
+    @user = user
+    @project = project
+    @post = post
+    
+
+     mail(to: "#{user.name} <#{user.email}>", subject: "New post #{post.title.capitalize} for #{project.name.capitalize} | Ativa")
+  end
 end
