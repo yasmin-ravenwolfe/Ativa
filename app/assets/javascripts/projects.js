91ed4eda32712fa09950dd709b641888afef4c5b
MakerSquare Student <students@themakersquare.com>
2013-10-17 12:09:04 -0500

    Try to restructure _show so can call sibling of tr in projects.js

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,3 +1,9 @@
 $(document).on('ajax:success', function(e, data) {
   console.log('Ajax Response data:', data);
 });
+
+$('project-row.button').click(function(){
+  // sibling is hidden
+  $('.hiddenform').show();
+  console.log('projects.js is working');
+});
